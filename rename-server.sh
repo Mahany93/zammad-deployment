d977656cd7a7195ca4f1b2f44bc1b1cfe86e2d1c
Mahmoud Mahany <mahmoud.mahany1@gmail.com>
2024-08-12 14:22:51 +0300

    modify rename script

--- rename-server.sh
+++ rename-server.sh
@@ -6,11 +6,11 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
-# Check if a new hostname is provided as an argument
-if [ -z "$1" ]; then
-  echo "Usage: $0 <new_hostname>"
-  exit 1
-fi
+# # Check if a new hostname is provided as an argument
+# if [ -z "$1" ]; then
+#   echo "Usage: $0 <new_hostname>"
+#   exit 1
+# fi
 
 NEW_HOSTNAME=${1:-zammad01.home.local}
 
